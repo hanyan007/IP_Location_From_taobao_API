cf3450349218debe70a59e424eb4371e63262677
root <root@instance-e3fvd5z6.(none)>
2016-10-13 22:52:24 +0800

    readme

--- ip_taobao.py
+++ ip_taobao.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Date    : 2016-10-21 
 # @Author  : Bruce (hanyan_007@139.com)
@@ -7,12 +8,10 @@ import optparse
 import  sys
 import urllib2
 
-
 usage="""%prog <ip>
 Samples:
     %prog 8.8.8.8 """
 
-#from optparse import OptionParser  
 def main():
     parser = optparse.OptionParser(usage = usage)
 
@@ -29,9 +28,6 @@ def main():
 	res = urllib2.urlopen("http://ip.taobao.com//service/getIpInfo.php?ip="+ip)
 	a = res.read()
 	zidian = eval(a)	
-#	print zidian
-	#print zidian.decode("unicode_escape")
-#	print ip
 	country=zidian['data']['country'].decode("unicode_escape")
 	area=zidian['data']['area'].decode("unicode_escape")
 	region=zidian['data']['region'].decode("unicode_escape")
@@ -42,12 +38,12 @@ def main():
 	print ip,isp,city,region,area,country
 
 
-
     except Exception, e:
-        print "ThreatBook returned a non correct response."
+        print "ip.taobao.com returned a non correct response."
 
 if __name__ == "__main__":
     main()
+
 
 
 
